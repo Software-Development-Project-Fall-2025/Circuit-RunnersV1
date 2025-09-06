177cd579b7a51fbf578da554861b33880a346fbb
Conner Brunner <connerbrunner@gmail.com>
2025-09-06 15:48:11 -0400

    Car Physics
    I implemented a simple set of car physics in a new scene because unity had me save it as a new scene. I think I simply didn't enter the scene with the cube.

---
+++ CircuitRunners/Assets/Scripts/CarController.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public enum Axel
+    {
+        Front,
+        Rear
+    }
+
+    [System.Serializable]
+    public struct Wheel
+    {
+        public GameObject wheelModel;
+        public WheelCollider wheelCollider;
+        public Axel axel;
+    }
+
+    public float maxAcceleration = 30f;
+    public float brakeAcceleration = 50f;
+    public float turnSensitivity = 1f;
+    public float maxSteeringAngle = 30f;
+
+    public Vector3 centerOfMass;
+
+    public List<Wheel> wheels;
+
+    float moveInput;
+    float steerInput;
+
+    private Rigidbody carRb;
+
+    void Start()
+    {
+        carRb = GetComponent<Rigidbody>();
+        carRb.centerOfMass = centerOfMass;
+    }
+
+    void GetInputs()
+    {
+        moveInput = Input.GetAxis("Vertical");
+        steerInput = Input.GetAxis("Horizontal");
+    }
+
+    void Update()
+    {
+        GetInputs();
+    }
+
+    void LateUpdate()
+    {
+        Move();
+        Steer();
+    }
+
+    void Move()
+    {
+        foreach (var wheel in wheels)
+        {
+            wheel.wheelCollider.motorTorque = moveInput * 600 * maxAcceleration * Time.deltaTime;
+        }
+    }
+
+    void Steer()
+    {
+        foreach (var wheel in wheels)
+        {
+            if (wheel.axel == Axel.Front)
+            {
+                float steerAngle = steerInput * maxSteeringAngle * turnSensitivity;
+                wheel.wheelCollider.steerAngle = Mathf.Lerp(wheel.wheelCollider.steerAngle, steerAngle, 0.6f);
+            }
+        }
+    }
+}
