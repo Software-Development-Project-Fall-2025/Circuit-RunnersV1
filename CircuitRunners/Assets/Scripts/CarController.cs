7fa358e37952ef9e5b79f0ecfd9da22a25b8f918
Zach <zschupba@kent.edu>
2025-09-17 13:27:05 -0400

    commit before changing car again

--- CircuitRunners/Assets/Scripts/CarController.cs
+++ CircuitRunners/Assets/Scripts/CarController.cs
@@ -4,76 +4,25 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    private Vector3 input;
-    void GatherInputs()
-    {
-        moveInput = Input.GetAxis("Vertical");
-        steerInput = Input.GetAxis("Horizontal");
-        input = new Vector3(steerInput, 0, moveInput);
-    }
-    
-    public enum Axel
-    {
-        Front,
-        Rear
-    }
+    //private float moveInput;
 
-    [System.Serializable]
-    public struct Wheel
-    {
-        public GameObject wheelModel;
-        public WheelCollider wheelCollider;
-        public Axel axel;
-    }
+    //public float fwdSpeed;
 
-    public float maxAcceleration = 30f;
-    public float brakeAcceleration = 50f;
-    public float turnSensitivity = 1f;
-    public float maxSteeringAngle = 30f;
-
-    public Vector3 centerOfMass;
-
-    public List<Wheel> wheels;
-
-    float moveInput;
-    float steerInput;
-
-    private Rigidbody carRb;
-
+    public Rigidbody sphereRB;
     void Start()
     {
-        carRb = GetComponent<Rigidbody>();
-        carRb.centerOfMass = centerOfMass;
+
     }
 
     void Update()
     {
-        GatherInputs();
+        //moveInput = Input.GetAxisRaw("Vertical");
+        transform.position = sphereRB.transform.position;
+        //moveInput *= fwdSpeed; 
     }
 
-    void LateUpdate()
-    {
-        Move();
-        Steer();
-    }
+    // private void FixedUpdate() {
+    //     sphereRB.AddForce(transform.forward * moveInput, ForceMode.Acceleration);
+    // }
 
-    void Move()
-    {
-        foreach (var wheel in wheels)
-        {
-            wheel.wheelCollider.motorTorque = moveInput * 600 * maxAcceleration * Time.deltaTime;
-        }
-    }
-
-    void Steer()
-    {
-        foreach (var wheel in wheels)
-        {
-            if (wheel.axel == Axel.Front)
-            {
-                float steerAngle = steerInput * maxSteeringAngle * turnSensitivity;
-                wheel.wheelCollider.steerAngle = Mathf.Lerp(wheel.wheelCollider.steerAngle, steerAngle, 0.6f);
-            }
-        }
-    }
 }
