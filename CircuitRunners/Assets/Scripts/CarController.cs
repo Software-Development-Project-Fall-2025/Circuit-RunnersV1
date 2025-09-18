e07f0e9e8b49a76296f894f18c02a032f6676590
Zach <zschupba@kent.edu>
2025-09-18 00:26:46 -0400

    basic ahh car finished

--- CircuitRunners/Assets/Scripts/CarController.cs
+++ CircuitRunners/Assets/Scripts/CarController.cs
@@ -2,27 +2,59 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Credit for base mechanics 
+// https://www.youtube.com/watch?v=TBIYSksI10k
+
+/*
+Mechanics I want
+Can turn really well at lower speeds, higher speeds oversteer
+When you turn too much at higher speed (after oversteering/drifting a bunch) you spin out
+Gradual Acceleration
+Variables to easily control everything
+*/
 public class CarController : MonoBehaviour
 {
-    //private float moveInput;
+    // Inputs from user
+    private float moveInput;
+    private float turnInput;
 
-    //public float fwdSpeed;
+    // Static car Variables (Stats)
+    public float fwdSpeed = 200;  
+    //public float[] gearSpeeds = {-50, 25, 50, 75, 60, 40};
+    //public float maxSpeed = 200;
+    public float turnSpeed = 200;
+
+    // Dynamic car variables for easy access
+    private float currentSpeed;
+    private float currentTurnAngle;
+
 
     public Rigidbody sphereRB;
     void Start()
     {
-
+        sphereRB.transform.parent = null;
     }
 
     void Update()
-    {
-        //moveInput = Input.GetAxisRaw("Vertical");
+    {   
+        // need to put code for adjustable speed in here
+
+        moveInput = Input.GetAxisRaw("Vertical");
+        turnInput = Input.GetAxisRaw("Horizontal");
+
+
         transform.position = sphereRB.transform.position;
-        //moveInput *= fwdSpeed; 
+        // Meldin gonna kill me for this >_> (ill optimize later)
+        // if()
+        moveInput *= fwdSpeed; 
+        
+        float newRotation = turnInput * turnSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical");
+        transform.Rotate(0,newRotation,0 ,Space.World);
+
     }
 
-    // private void FixedUpdate() {
-    //     sphereRB.AddForce(transform.forward * moveInput, ForceMode.Acceleration);
-    // }
+    private void FixedUpdate() {
+        sphereRB.AddForce(transform.forward * moveInput, ForceMode.Acceleration);
+    }
 
 }
